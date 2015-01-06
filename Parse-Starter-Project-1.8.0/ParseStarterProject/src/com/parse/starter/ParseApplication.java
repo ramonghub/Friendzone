d2f50c940cb6e759cb7c180b06310a17fc4b271a
ramonghub <RamonProBook@wcw-stud-145-109-22-173.wireless.uva.nl>
2015-01-06 11:30:25 +0100

    Android Studio Test
    Test version with parse included

---
+++ Parse-Starter-Project-1.8.0/ParseStarterProject/src/com/parse/starter/ParseApplication.java
@@ -0,0 +1,32 @@
+package com.parse.starter;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseCrashReporting;
+import com.parse.ParseUser;
+
+public class ParseApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+
+    // Initialize Crash Reporting.
+    ParseCrashReporting.enable(this);
+
+    // Add your initialization code here
+    Parse.initialize(this, YOUR_APPLICATION_ID, YOUR_CLIENT_KEY);
+
+
+    ParseUser.enableAutomaticUser();
+    ParseACL defaultACL = new ParseACL();
+      
+    // If you would like all objects to be private by default, remove this line.
+    defaultACL.setPublicReadAccess(true);
+    
+    ParseACL.setDefaultACL(defaultACL, true);
+  }
+}
